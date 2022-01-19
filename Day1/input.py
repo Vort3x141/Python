06b47931f153c5072edf0f973cd9d18effe7149b
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-01-19 23:16:59 +0530

    init

---
+++ Day1/input.py
@@ -0,0 +1,6 @@
+from typing import Concatenate
+
+
+print("Hello " + input ("Whats your name?") + "!")
+# Going inside Print statem then Checking "Hello" then taking a look at input function
+# asking user for input() and then Concatenating everything
