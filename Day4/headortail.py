b185a2f70636de80cab3ea72f55d7bbe9e8042b8
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-02-09 19:26:36 +0530

    FINAL

---
+++ Day4/headortail.py
@@ -0,0 +1,9 @@
+import random
+
+toss = random.randint(0,1)
+print(toss)
+
+if toss == 0:
+    print("Tails")
+else:
+    print("Heads")
---
+++ Day4/rand.py
@@ -0,0 +1,8 @@
+from operator import imod
+import random
+
+random_integer = random.randint(1, 100)
+print(random_integer)
+
+random_float = random.random() * 5
+print(random_float)
