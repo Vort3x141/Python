c3f801a703e2aabcc48e30be31a6d45de46ad2e0
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-06 00:53:12 +0530

    FINAL

--- Day5/averageheigh.py
+++ Day5/averageheigh.py
@@ -1,6 +1,3 @@
-from re import S
-
-
 students_heights = input("Input a list of students heights ").split()
 for n in range (0, len(students_heights)):
     students_heights[n] = int(students_heights[n])
---
+++ Day5/fizzbuzz.py
@@ -0,0 +1,14 @@
+# 3//fizz
+# 5//buzz
+# both fizzbuzz
+
+for i in range(1,101):
+    if i%3==0:
+        print("Fizz")
+    elif i%5==0:
+        print("Buzz")
+    elif i%3 and i%5==0:
+        print("FizzBuzz")
+    else:
+        print(i)
+    
