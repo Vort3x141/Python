f3ec55e13df59a25e536d9174b17385da1c7d4f3
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-25 20:34:12 +0530

    FINAL

---
+++ Day6/function.py
@@ -0,0 +1,10 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function() 
+#Use the function name to call it and provide the necessary input to the functuion(here)
---
+++ Day6/whileloops.py
@@ -0,0 +1,6 @@
+#general syntax of while loops
+
+# while something_is_true:
+    # Do this
+    # Then do this
+    # Then do this
