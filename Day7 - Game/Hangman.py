de8fa983a8ef3afffbff7f341ae32b878ea29f56
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-26 22:20:58 +0530

    FINAL

--- Day7 - Game/Hangman.py
+++ Day7 - Game/Hangman.py
@@ -10,14 +10,19 @@ for i in range(len(chosen_word)):
     display.append("_")
 print(display)
 #append _ in the empty list which is equal to the length of the choosen word. 
+End_of_game = False
 
+while not End_of_game:
+    guess = input("Guess a letter: ").lower()
+    #inputing a letter and converting it into a lower case
 
-guess = input("Guess a letter: ").lower()
-#inputing a letter and converting it into a lower case
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    #using a for loop to check & compare each letter of the chosen word and the guessed word
+    print(display)
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-#using a for loop to check & compare each letter of the chosen word and the guessed word
-print(display)
+    if "_" not in display:
+        End_of_game = True
+        print("Jeet gya bsdk")
