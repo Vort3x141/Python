17ccc2539e0d8d4d2c35ace9bde6267876d415f4
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-26 23:52:04 +0530

    FINAL

--- Day7 - Game/Hangman.py
+++ Day7 - Game/Hangman.py
@@ -1,8 +1,14 @@
 import random
+import hangman_words
+import hangman_art
 
+lives = 6
 
-word_list = ["aardvark", "baboon", "camel"]
+print(hangman_art.logo)
+
+word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
+
 #Choosing a random word from the list
 #choosen word count 
 display = []
@@ -16,13 +22,27 @@ while not End_of_game:
     guess = input("Guess a letter: ").lower()
     #inputing a letter and converting it into a lower case
 
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+    
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. you loose a life")
+        lives -=1
+        if lives == 0:
+            End_of_game = True
+            print("You loose")
+    
     #using a for loop to check & compare each letter of the chosen word and the guessed word
     print(display)
 
     if "_" not in display:
         End_of_game = True
-        print("Jeet gya bsdk")
+        print("You win")
+
+    print(hangman_art.stages[lives])
+        
