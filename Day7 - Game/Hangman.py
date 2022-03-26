946da3701ed796d292c4349b43776160dc911f25
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-03-26 19:45:57 +0530

    FINAL

---
+++ Day7 - Game/Hangman.py
@@ -0,0 +1,23 @@
+import random
+
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+#Choosing a random word from the list
+#choosen word count 
+display = []
+for i in range(len(chosen_word)):
+    display.append("_")
+print(display)
+#append _ in the empty list which is equal to the length of the choosen word. 
+
+
+guess = input("Guess a letter: ").lower()
+#inputing a letter and converting it into a lower case
+
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+#using a for loop to check & compare each letter of the chosen word and the guessed word
+print(display)
