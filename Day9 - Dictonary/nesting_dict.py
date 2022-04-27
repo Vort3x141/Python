e8644c22375865bb5cf8c8f8a19949079757fbb1
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-04-27 13:24:28 +0530

    FINAL

---
+++ Day9 - Dictonary/nesting_dict.py
@@ -0,0 +1,14 @@
+#Nesting
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Little", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5}
+}
